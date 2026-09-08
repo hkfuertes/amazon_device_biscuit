743b7adc43f0559ed993b352ba4f7784a417c631
Miguel Fuertes <mjfuertesf@gmail.com>
2026-09-08 12:11:50 +0200

    cm12: decouple bootstrap base from echolocal

--- device/amazon/biscuit/biscuit_bootstrap.mk
+++ device/amazon/biscuit/biscuit_bootstrap.mk
@@ -1,4 +1,4 @@
-# Frameworkless Biscuit bootstrap product.
+# Frameworkless Biscuit base product.
 # It intentionally does not inherit full_base, core_minimal, core_tiny, or CM common.
 
 $(call inherit-product, device/amazon/biscuit/biscuit_bootstrap_device.mk)
@@ -6,12 +6,7 @@ $(call inherit-product, device/amazon/biscuit/biscuit_bootstrap_device.mk)
 PRODUCT_NAME         := biscuit_bootstrap
 PRODUCT_DEVICE       := biscuit
 PRODUCT_BRAND        := Amazon
-PRODUCT_MODEL        := Echo Dot Bootstrap
+PRODUCT_MODEL        := Echo Dot Minimal Base
 PRODUCT_MANUFACTURER := amazon
 
 PRODUCT_CHARACTERISTICS := nosdcard
-
-# The future installer uses this to select its standalone service path.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.echolocal.install_mode=standalone \
-    ro.echolocal.service=echod
